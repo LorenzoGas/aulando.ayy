6a8d4e9659a8628b57476223e3c7c2d1aa03d785
Riccardo <riccardo.martinelli96@gmail.com>
2017-11-26 14:28:03 +0100

    modifica su bonificaDatiAule, updateAule
    bonificaDatiAule restituisce una json, quasi fatto updateAule

--- db/BonificaDatiAule/bonificaDatiAule.js
+++ db/BonificaDatiAule/bonificaDatiAule.js
@@ -1,5 +1,5 @@
 fs = require('fs')
-var output  = fs.createWriteStream('output.csv', {
+var output  = fs.createWriteStream('dati.json', {
   flags: 'a' // 'a' means appending (old data will be preserved)
 })
 fs.readFile('dati.csv', 'utf8', function (err,data) {
@@ -8,46 +8,45 @@ fs.readFile('dati.csv', 'utf8', function (err,data) {
   }
   
   var list = data.split('\n');
+
+  var result = [
+    {
+      entries: [],
+      name: "Aula"
+    }
+  ];
+
   list.forEach(function(element) {
       element = element.replace("\r", "")
       element = element.replace("  ", " ")
-      var result = [];
-      result.push(element);
-      result.push(element);
+      element = element.replace('"', '')
+      element = element.replace('"', '')
+      var synonyms = [];     
 
-      pulisci(result, element, ["Aula "]);
-      pulisci(result, element, ["Aula pc "]);
+      synonyms.push(element);
 
-      pulisci(result, element, [" - Andreatta", "Aula "]);
-      pulisci(result, element, [" (SALA CONFERENZE)", "Aula "]);
-      pulisci(result, element, [" piano terra", "Aula "]);
-      pulisci(result, element, [" primo piano", "Aula "]);
-      pulisci(result, element, [" piano terra", "Aula "]);
-      pulisci(result, element, [" (spazio polifunzionale)", "Aula "]);
-      pulisci(result, element, [" - 1Â° p.", "Aula "]);
-      pulisci(result, element, [" - P.T.", "Aula "]);
+      pulisci(synonyms, element, ["Aula "]);
+      pulisci(synonyms, element, ["Aula pc "]);
 
-     /* result.push(pulisci(element, [" - Andreatta", "Aula "]));
-      result.push(pulisci(element, [" (SALA CONFERENZE)", "Aula "]));
-      result.push(pulisci(element, [" piano terra", "Aula "]));
-      result.push(pulisci(element, [" primo piano", "Aula "]));
-      result.push(pulisci(element, [" piano terra", "Aula "]));
-      result.push(pulisci(element, [" (spazio polifunzionale)", "Aula "]));
-      result.push(pulisci(element, [" - 1Â° p.", "Aula "]));
-      result.push(pulisci(element, [" - P.T.", "Aula "]));
+      pulisci(synonyms, element, [" - Andreatta", "Aula "]);
+      pulisci(synonyms, element, [" (SALA CONFERENZE)", "Aula "]);
+      pulisci(synonyms, element, [" piano terra", "Aula "]);
+      pulisci(synonyms, element, [" primo piano", "Aula "]);
+      pulisci(synonyms, element, [" piano terra", "Aula "]);
+      pulisci(synonyms, element, [" (spazio polifunzionale)", "Aula "]);
+      pulisci(synonyms, element, [" - 1Â° p.", "Aula "]);
+      pulisci(synonyms, element, [" - P.T.", "Aula "]);
 
-      result.push(pulisci(element, ["Aula "]));
-      result.push(pulisci(element, ["Aula pc "])); */
+      var entries = 
+            {
+              "synonyms": synonyms,
+              "value": synonyms[0]
+            }
 
-
-      var text = "";
-      text += result[0]
-      for(var i=1; i < result.length; i++){
-        text += ", " + result[i]
-      }
-      text += "\n"
-      output.write(text)
+      result[0].entries.push(entries)
   });
+
+  output.write(JSON.stringify(result))
 });
 
 
