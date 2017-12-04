a95895e23b0e00eefd89f05e6ea44f93895bcd2a
Unknown <alvise.sembenico@studenti.unitn.it>
2017-12-04 18:22:37 +0100

    Chat resize

--- front end/script.js
+++ front end/script.js
@@ -1,3 +1,20 @@
+function chatResize(container){
+    var cheigth=container.offset().top - $(window).scrollTop()
+    console.log($(window).height());
+    console.log(cheigth);
+    container.height($(window).height()-cheigth-200);
+
+}
+$(window).resize(function(){
+    var container=$("#chat-container");
+    chatResize(container);
+});
+$(document).ready(function(){
+    var container=$("#chat-container");
+    chatResize(container);
+});
+
+
 String.prototype.splice = function (idx, rem, str) {
     return this.slice(0, idx) + str + this.slice(idx + Math.abs(rem));
 };
@@ -5,7 +22,7 @@ String.prototype.splice = function (idx, rem, str) {
 $("#userInput").change(startProcedure);
 
 function startProcedure() {
-    var value=$("#userInput").val();
+    var value = $("#userInput").val();
     send(value);
     appentNewText(value, $("#message-send-text"));
     $("#userInput").val("");
@@ -17,7 +34,7 @@ function appentNewText(txt, source) {
     var hours = new Date().getHours();
     msg = msg.replace("§", hours + ":" + minutes);
     $(msg).appendTo("#chat-container");
-    var div= $('#chat-container');
+    var div = $('#chat-container');
     div.scrollTop(div.prop('scrollHeight'));
 }
 
@@ -25,17 +42,17 @@ function send(txt) {
     var cookie;
     var session = -1;
     if (session < 0) {
-        var jqxhr = $.get("https://aulando-ayy-dialogflow-api.herokuapp.com/dialogflow_api/resolveQuery?requestQuery=" + txt, function () {})
+        var jqxhr = $.get("https://aulando-ayy-dialogflow-api.herokuapp.com/dialogflow_api/resolveQuery?requestQuery=" + txt, function () { })
             .done(function (res) {
                 manageRes(res);
-                session=1;
-                cookie=res.sessionId;
+                session = 1;
+                cookie = res.sessionId;
             })
             .fail(function (res) {
                 $('#error').modal();
             });
     } else {
-        var jqxhr = $.get("https://aulando-ayy-dialogflow-api.herokuapp.com/dialogflow_api/resolveQuery?requestQuery=" + txt+"&id="+cookie, function () {})
+        var jqxhr = $.get("https://aulando-ayy-dialogflow-api.herokuapp.com/dialogflow_api/resolveQuery?requestQuery=" + txt + "&id=" + cookie, function () { })
             .done(function (res) {
                 manageRes(res);
             })
@@ -45,14 +62,18 @@ function send(txt) {
     }
 }
 
-function manageRes(res){
-    var text=res.speech;
-    if(res.result){
-        text+="<ul>";
-        res.result.forEach(function(item,i){
-            text+="<li>"+item.nome+"</li>";
-        });
-        text+="</ul>";
+function manageRes(res) {
+    var text = res.speech;
+    if (res.result) {
+        if (res.result != null) {
+            text += "<ul>";
+            res.result.forEach(function (item, i) {
+                if (item.fino != null)
+                    text += item.nome + " fino alle <b>" + item.fino + "</b><br>";
+                else
+                    text += item.nome + "<br>";
+            });
+        }
     }
-    appentNewText(text,$("#message-receive-text"));
+    appentNewText(text, $("#message-receive-text"));
 }
